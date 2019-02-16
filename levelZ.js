df06ab497e9eea767bab32b3666fa79f828e18e4
Joshua Humphrey <joshhumphrey1@gmail.com>
2019-02-16 01:23:18 -0600

    initial commit

---
+++ levelZ.js
@@ -0,0 +1,12 @@
+function openSlideMenu() {
+	document.getElementById('bar').style.display='block';
+	document.getElementById('open').style.display='none';
+}
+
+
+
+function closeSlideMenu() {
+	document.getElementById('bar').style.display='none';
+	document.getElementById('open').style.display='block';
+}
+
